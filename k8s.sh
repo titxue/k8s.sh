0131fbb983a60624edea5df4fd817d9109ee18ef
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-07-17 15:23:33 +0800

    :wrench: 使用码云替换极狐

--- k8s.sh
+++ k8s.sh
@@ -1019,7 +1019,7 @@ function _calico_init() {
       curl -o calico.yaml "$calico_manifests_mirror"
     else
       echo -e "${COLOR_BLUE}kubernetes 网络插件 calico 使用版本：${COLOR_RESET}${COLOR_GREEN}${calico_version}${COLOR_RESET}"
-      curl -o calico.yaml https://jihulab.com/xuxiaowei-jihu/mirrors-github/projectcalico/calico/-/raw/v"$calico_version"/manifests/calico.yaml
+      curl -o calico.yaml https://gitee.com/mirrors-github/calico/blob/v"$calico_version"/manifests/calico.yaml
     fi
 
     # 网卡
@@ -1098,7 +1098,7 @@ _ingress_nginx_install() {
     echo -e "${COLOR_BLUE}Ingress Nginx 插件 安装开始${COLOR_RESET}"
 
     echo -e "${COLOR_BLUE}Ingress Nginx 插件 下载配置文件${COLOR_RESET}"
-    curl -o deploy.yaml https://jihulab.com/xuxiaowei-jihu/mirrors-github/kubernetes/ingress-nginx/-/raw/controller-v$ingress_nginx_version/deploy/static/provider/cloud/deploy.yaml
+    curl -o deploy.yaml https://gitee.com/mirrors-github/ingress-nginx/raw/controller-v$ingress_nginx_version/deploy/static/provider/cloud/deploy.yaml
 
     echo -e "${COLOR_BLUE}Ingress Nginx 插件 修改镜像${COLOR_RESET}"
     sudo sed -i 's/@.*$//' deploy.yaml
